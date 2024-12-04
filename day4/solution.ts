8ab32f0f0e10da0271bf9fb4a59f4023be63589e
karlflores <karl.flores.9@gmail.com>
2024-12-04 19:19:59 +1100

    unoptimized solution for day4

--- day4/solution.ts
+++ day4/solution.ts
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs'
 type ValidChars = "X" | "M" | "A" | "S";
 type Word = ValidChars[];
 type Crossword = Word[];
-type ValidWord = "XMAS" | "SAMX";
+type ValidWord = "XMAS" | "SAMX" | "SAM" | "MAS";  
 
 const NEWLINE = "\n"
 
@@ -18,6 +18,21 @@ function countValidWordsInWord(word: Word) : number {
     }
 
     return numFound;
+}
+
+function isValidCrossMas(row: number, column: number, crossword: Crossword) : boolean {
+    if(row+2 >= crossword.length || column+2 >= crossword[0].length){
+        return false;
+    }
+
+    if(crossword[row+1][column+1] !== 'A'){
+        return false;
+    }
+ 
+    // now we can test the MAS or SAM on the diagonals one by one  
+    const downRightValid = (crossword[row][column] === 'M' && crossword[row+2][column+2] === 'S' || crossword[row][column] === 'S' && crossword[row+2][column+2] === 'M' );
+    const downLeftValid = (crossword[row][column+2] === 'S' && crossword[row+2][column] === 'M' || crossword[row][column+2] === 'M' && crossword[row+2][column] === 'S' );
+    return downLeftValid && downRightValid;
 }
 
 function getColumn(column: number, crossword: Crossword) : Word {
@@ -76,33 +91,49 @@ async function parseInputFile(path: string): Promise<Crossword> {
   return crossword;
 }
 
+function findValidWords(crossword: Crossword) : number {
+    let numWords = crossword.reduce((acc, r) => acc + countValidWordsInWord(r), 0);
+
+    // get row words
+    numWords += crossword[0].reduce((acc, _, i) => {
+        const col = getColumn(i, crossword);
+        return acc + countValidWordsInWord(col);
+    }, 0);
+
+    // now we have to do the diagonals -- do down right diag first
+    for(let i = 1 ; i < crossword.length  ; i++){
+        const word = getDownRightDiagonal(i, 0, crossword);
+        numWords += countValidWordsInWord(word);
+    }
+
+
+    // now do the rows 
+    numWords += crossword[0].reduce((acc, _, i) => acc + countValidWordsInWord(getDownRightDiagonal(0,i,crossword)) + countValidWordsInWord(getDownLeftDiagonal(0,i,crossword)),0);
+
+    // do the last diagonal
+    for(let i = 1 ; i < crossword.length  ; i++){
+        const word = getDownLeftDiagonal(i, crossword.length-1, crossword);
+        numWords += countValidWordsInWord(word);
+    }
+
+    return numWords;
+}
+
+function findCrossMas(crossword: Crossword) : number {
+    let numValid = 0;
+    for(let row = 0 ; row + 2 < crossword.length ; row++) {
+        for(let col = 0 ; col + 2 < crossword[0].length ; col++) {
+            // now test the cross
+            numValid += isValidCrossMas(row,col, crossword) ? 1 : 0;
+        }
+    }
+
+    return numValid;
+}
+
 export default function solution(){
     parseInputFile('day4/input.txt').then((data => {
-        // get row words
-        let numWords = data.reduce((acc, r) => acc + countValidWordsInWord(r), 0);
-
-        // get row words
-        numWords += data[0].reduce((acc, _, i) => {
-            const col = getColumn(i, data);
-            return acc + countValidWordsInWord(col);
-        }, 0);
-
-        // now we have to do the diagonals -- do down right diag first
-        for(let i = 1 ; i < data.length  ; i++){
-            const word = getDownRightDiagonal(i, 0, data);
-            numWords += countValidWordsInWord(word);
-        }
-
-
-        // now do the rows 
-        numWords += data[0].reduce((acc, _, i) => acc + countValidWordsInWord(getDownRightDiagonal(0,i,data)) + countValidWordsInWord(getDownLeftDiagonal(0,i,data)),0);
-
-        // do the last diagonal
-        for(let i = 1 ; i < data.length  ; i++){
-            const word = getDownLeftDiagonal(i, data.length-1, data);
-            numWords += countValidWordsInWord(word);
-        }
-
-        console.log(numWords);
+        console.log("Part 1: ",findValidWords(data));
+        console.log("Part 2: ", findCrossMas(data));
     }));
 }
